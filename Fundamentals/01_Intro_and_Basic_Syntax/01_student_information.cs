e0616c0b39c7252561a8e46617d940a4cdde1604
ddimitrovv <dimitrov.mitko.md@gmail.com>
2024-04-03 13:26:48 +0300

    added tasks

---
+++ Fundamentals/01_Intro_and_Basic_Syntax/01_student_information.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+            Console.WriteLine("Name: {0}, Age: {1}, Grade: {2:F2}", name, age, grade);
+        }
+    }
+}
