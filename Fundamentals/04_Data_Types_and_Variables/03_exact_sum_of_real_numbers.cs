81504f5dd44dff58961040b61038fc19ad7554f9
ddimitrovv <dimitrov.mitko.md@gmail.com>
2024-04-08 10:15:16 +0300

    added tasks

---
+++ Fundamentals/04_Data_Types_and_Variables/03_exact_sum_of_real_numbers.cs
@@ -0,0 +1,16 @@
+using System;
+
+class ExactSumOfRealNumbers
+{
+
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        decimal sum = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += decimal.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(sum);
+    }
+}
